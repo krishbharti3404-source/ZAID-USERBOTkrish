e4dcd59f3a2f622a77a53ea72ae1a3fb6c7e241e
krishbharti3404-source <krishbharti3404@gmail.com>
2025-11-07 01:47:47 +0530

    Update API credentials and configuration values

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d") #optional
+API_ID = int(getenv("API_ID", "26327185")) #optional
+API_HASH = getenv("API_HASH", "d0d7099c37fe6d9ecab8ec16836b5233") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID"))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3c52a01057865f7511168.jpg')
+OWNER_ID = getenv("OWNER_ID"," 7519668949")
+MONGO_URL = getenv("MONGO_URL","mongodb+srv://YoruMusic:Yoruu@yoru.ku15xlh.mongodb.net/?appName=Yoru")
+BOT_TOKEN = getenv("BOT_TOKEN", "8052618301:AAEPVDCjd4eYNN7pwTLGs6mp8aK9ESv_e9E")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/0pngkf.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://t.me/Fine_N_Ok")
+REPO_URL = getenv("REPO_URL", "https://t.me/ITsExclusive69")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
